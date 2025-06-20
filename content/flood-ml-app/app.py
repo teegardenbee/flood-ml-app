c7c804d64408cf3eb8812574c4f0d72fb4ba8e5d
Atharva Dandagawhal <adandagawhal@gmail.com>
2025-06-20 23:09:52 +0530

    Update app.py

--- content/flood-ml-app/app.py
+++ content/flood-ml-app/app.py
@@ -10,9 +10,9 @@ option = st.radio("Choose a map to view:", ["Flood Prediction", "Elevation", "Sl
 if option == "Flood Prediction":
     st.image("content/flood-ml-app/flood_prediction.png", caption="Predicted Flood Risk", use_container_width=False)
 elif option == "Elevation":
-    st.image("content/flood-ml-app/elevation_map.png", caption="Elevation Map", use_container_width=True)
+    st.image("content/flood-ml-app/elevation_map.png", caption="Elevation Map", use_container_width=False)
 elif option == "Slope":
-    st.image("content/flood-ml-app/slope_map.png", caption="Slope Map", use_container_width=True)
+    st.image("content/flood-ml-app/slope_map.png", caption="Slope Map", use_container_width=False)
 
 st.markdown("---")
 st.markdown("🛰️ Based on real DEM data and a PyTorch ML model.")
