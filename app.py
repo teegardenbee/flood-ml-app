09d33d544b69e05e923416e1bad1e0c5235f709a
Atharva Dandagawhal <adandagawhal@gmail.com>
2025-06-26 19:26:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,17 +85,4 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# ---- Custom Thin Line Footer ----
-st.markdown("<hr style='margin-top: 5px; margin-bottom: 5px;'>", unsafe_allow_html=True)
-st.markdown("""
-    <div style='text-align: center; font-size: 0.95rem; color: gray;'>
-        Developed by - Atharva Dandagawhal |
-        <a href='https://github.com/teegardenbee' target='_blank' style='color: gray; text-decoration: none;'>
-            @teegardenbee
-        </a>
-    </div>
-""", unsafe_allow_html=True)
-
-
-
-
+st.caption("Developed by - Atharva Dandagawhal | [@teegardenbee](https://github.com/teegardenbee)")
